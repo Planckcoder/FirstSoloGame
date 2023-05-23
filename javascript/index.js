2b9911ae280f2c7d1d23b23534df3f6a040889ba
PlanckCoder <jvitorrh1@gmail.com>
2023-05-23 17:31:58 +0100

    added player class

---
+++ javascript/index.js
@@ -0,0 +1,28 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+const player = canvas.getContext('2d')
+
+canvas.width = 1024;
+canvas.height = 576;
+
+
+
+const player1 = new Player()
+
+
+
+// let bottom = y + height
+
+function animate() {
+    window.requestAnimationFrame(animate)
+
+    c.fillStyle = 'white'
+    c.fillRect(0, 0, canvas.width, canvas.height);
+
+    player1.draw()
+    player1.update()
+
+
+}
+
+animate()
